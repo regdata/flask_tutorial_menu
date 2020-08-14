4e34600efe89b05747e65028cb5421fb9cf4181e
regdata <regdata.voice@gmail.com>
2020-08-14 01:52:05 -0400

    correct one-to-many relationship

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ db = SQLAlchemy(app)
 class Restaurants(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-
+        
     def __repr__(self):
-        return f'Restaurant {self.id}'
+        return f'Restaurant {self.name}'
 
 class MenuItems(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    rest_id = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
-
+    description = db.Column(db.Text, nullable=False)
+    
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'),nullable=False)
+    restaurant = db.relationship('Restaurants', backref=db.backref('menu_item', lazy=True))
+    
     def __repr__(self):
-        return f'Menu Item {self.id}'
+        return f'Menu Item {self.name}'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def restaurant_list():
@@ -50,6 +53,21 @@ def edit(id):
         return redirect('/')
     else:
         return render_template('edit.html', restaurant = rest)
+
+@app.route('/<int:id>', methods=['GET', 'POST'])
+def menu_items_list(id):
+    all_menu_items = MenuItems.query.filter_by(rest_id=id).all()
+    rest = Restaurants.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        menu_item_name = request.form['name']
+        menu_item_description = request.form['description']
+        new_menu_item = MenuItems(rest_id=id, name=menu_item_name, description=menu_item_description)
+        db.session.add(new_menu_item)
+        db.session.commit()
+        return redirect(f'/{id}')
+    
+    return render_template('menu_items.html', restaurant=rest, menu_items=all_menu_items)
 
 
 if __name__ == "__main__":
